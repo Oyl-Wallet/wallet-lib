ebea665a6994a0aacc1d789cda150a85656f1578
Eric Butz <ericgbutz@gmail.com>
2024-05-10 13:48:43 -0600

    Rune balance regtest method

--- lib/tests/regtestApi.d.ts
+++ lib/tests/regtestApi.d.ts
@@ -6,3 +6,7 @@ export declare const getRuneOutpointsRegtest: (address: string) => Promise<{
     statusCode: number;
     data: any[];
 }>;
+export declare const getRuneBalanceRegtest: (address: string) => Promise<{
+    statusCode: number;
+    data: any[];
+}>;
